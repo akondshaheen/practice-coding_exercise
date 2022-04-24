2ee5eb4ce68a8666f570ce422a81406f58c57dca
Shaheen Akond <shaheen.akond@roche.com>
2022-04-24 14:04:24 +0200

    discover of first and last position solutin one solved

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+import Practices.Find_first_and_last_position;
 import Practices.Practice1_Anagram;
 
 public class Main{
-    private static Practice1_Anagram practice1_Anagram;
-    public static void main(String[] args) {
-        practice1_Anagram.anagram("garden", "danger");
+    private static Practice1_Anagram practice1_Anagram = new Practice1_Anagram();
+    private static Find_first_and_last_position find_first_and_last_position = new Find_first_and_last_position();
+
+       public static void main(String[] args) {
+           practice1_Anagram.anagram("firstWord", "secondWord");
+           find_first_and_last_position.findPosition("", "x");
     }
 }
 
